0467da08c7225cee0d076e84fd3693d5ae820ffe
bourbonut <ben.bourbon06@gmail.com>
2025-04-02 21:18:45 +0200

    Update `extent.py` by using builtin functions such as `min` and `max`

--- detroit/array/extent.py
+++ detroit/array/extent.py
@@ -1,12 +1,10 @@
 import math
+from itertools import starmap
 from collections.abc import Callable, Iterable
 from inspect import signature
-from typing import TypeVar
-
-T = TypeVar("T")
+from ..types import T
 
 
-# TODO : use min and max instead of doing manually
 def extent(
     values: Iterable[T], accessor: Callable[[T, int, Iterable[T]], T] | None = None
 ) -> tuple[T, T]:
@@ -26,32 +24,21 @@ def extent(
     tuple[T, T]
         Minimum, maximum
     """
-    mini = None
-    maxi = None
-    if accessor is None:
-        for value in values:
-            if value is not None and (isinstance(value, str) or not math.isnan(value)):
-                if mini is None:
-                    mini = maxi = value
-                else:
-                    if mini > value:
-                        mini = value
-                    if maxi < value:
-                        maxi = value
-    else:
+
+    def is_valid(value):
+        """Check if the value is valid"""
+        return value is not None and (isinstance(value, str) or not math.isnan(value))
+
+    if accessor is not None:
         nargs = len(signature(accessor).parameters)
-        for index, value in enumerate(values):
+
+        def access(index, value):
+            """Access value given the accessor function"""
             args = [value, index, values][:nargs]
-            new_value = accessor(*args)
-            if new_value is not None and (
-                isinstance(new_value, str) or not math.isnan(new_value)
-            ):
-                value = new_value
-                if mini is None:
-                    mini = maxi = value
-                else:
-                    if mini > value:
-                        mini = value
-                    if maxi < value:
-                        maxi = value
-    return [mini, maxi]  # TODO: remove brackets and fix tests
+            return accessor(*args)
+
+        values = list(filter(is_valid, starmap(access, enumerate(values))))
+    else:
+        values = list(filter(is_valid, values))
+
+    return [min(values) if values else None, max(values) if values else None]
