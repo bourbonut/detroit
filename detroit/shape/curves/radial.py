d2902869982e34560b99ace5d6f1a911723e7426
bourbonut <ben.bourbon06@gmail.com>
2025-07-12 14:48:51 +0200

    Add `curve_radial_linear` to `shape/curve`

---
+++ detroit/shape/curves/radial.py
@@ -0,0 +1,32 @@
+from .linear import curve_linear
+from math import cos, sin
+
+class Radial:
+
+    def __init__(self, curve):
+        self._curve = curve
+
+    def area_start(self):
+        self._curve.area_start()
+
+    def area_end(self):
+        self._curve.area_end()
+
+    def line_start(self):
+        self._curve.line_start()
+
+    def line_end(self):
+        self._curve.line_end()
+    
+    def point(self, a, r):
+        self._curve.point(r * sin(a), -r * cos(a))
+
+
+def curve_radial(curve):
+    def radial(context):
+        return Radial(curve(context))
+
+    return radial
+
+def curve_radial_linear(context):
+    return curve_radial(curve_linear)
