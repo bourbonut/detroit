50fc394212385c88dca4a4b38db1e885b993643e
bourbonut <ben.bourbon06@gmail.com>
2025-08-29 19:25:37 +0200

    Refactor `all_options_axis.py` in `examples`

--- examples/all_options_axis.py
+++ examples/all_options_axis.py
@@ -6,203 +6,90 @@ width = 810
 height = 30
 margin = 15
 
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin]))
-x1 = svg.append("g").attr("transform", f"translate({margin}, {margin * 0.5})").call(
-    axis.set_ticks(3, "%")
-)
+theme = "light"
 
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
+axes = [
+    (
+        "axis_ticks_1",
+        d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin])).set_ticks(
+            3, "%"
+        ),
+    ),
+    (
+        "axis_ticks_2",
+        d3.axis_bottom(
+            d3.scale_time(
+                [datetime(2000, 1, 1, 12, 0), datetime(2000, 1, 1, 13, 0)], [0, 1]
+            ).set_range([0, width - 2 * margin])
+        ).set_ticks(d3.time_minute.every(15)),
+    ),
+    (
+        "axis_angle",
+        d3.axis_bottom(
+            d3.scale_linear().set_domain([0, 360]).set_range([0, width - 2 * margin])
+        ).set_tick_values([0, 120, 240, 360]),
+    ),
+    (
+        "axis_format",
+        d3.axis_bottom(
+            d3.scale_linear().set_domain([0, 10_000]).set_range([0, width - 2 * margin])
+        ).set_tick_format(d3.format("~s")),
+    ),
+    (
+        "axis_ticks_size",
+        d3.axis_bottom(
+            d3.scale_linear().set_range([0, width - 2 * margin])
+        ).set_tick_size(12),
+    ),
+    (
+        "axis_tick_size_inner",
+        d3.axis_bottom(
+            d3.scale_linear().set_range([0, width - 2 * margin])
+        ).set_tick_size_inner(0),
+    ),
+    (
+        "axis_ticks_size_outer",
+        d3.axis_bottom(
+            d3.scale_linear().set_range([0, width - 2 * margin])
+        ).set_tick_size_outer(12),
+    ),
+    (
+        "axis_tick_padding",
+        d3.axis_bottom(
+            d3.scale_linear().set_range([0, width - 2 * margin])
+        ).set_tick_padding(8),
+    ),
+    (
+        "axis_offset",
+        d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin])).set_offset(
+            8
+        ),
+    ),
+]
 
-# with open("light_axis_ticks_1.svg", "w") as file:
-    # file.write(str(svg))
 
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-scale = d3.scale_time(
-    [datetime(2000, 1, 1, 12, 0), datetime(2000, 1, 1, 13, 0)], [0, 1]
-)
-axis = d3.axis_bottom(scale.set_range([0, width - 2 * margin]))
-x2 = svg.append("g").attr("transform", f"translate({margin}, {margin * 0.5})").call(
-    axis.set_ticks(d3.time_minute.every(15))
-)
+def generate_svg(title, axis):
+    svg = (
+        d3.create("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", f"0 0 {width} {height}")
+    )
+    (
+        svg.append("g")
+        .attr("transform", f"translate({margin}, {margin * 0.5})")
+        .call(axis)
+    )
 
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
+    if theme == "dark":
+        svg.select_all("path.domain").attr("stroke", "white")
+        svg.select_all("g.tick line").attr("stroke", "white")
+        svg.select_all("g.tick text").attr("fill", "white").attr("stroke", "none")
+        svg.select_all("text").attr("fill", "white").attr("stroke", "none")
 
-# with open("light_axis_ticks_2.svg", "w") as file:
-    # file.write(str(svg))
+    with open(f"{theme}_{title}.svg", "w") as file:
+        file.write(str(svg))
 
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(
-    d3.scale_linear().set_domain([0, 360]).set_range([0, width - 2 * margin])
-)
-x3 = (
-    svg.append("g")
-    .attr("transform", f"translate({margin}, {margin * 0.5})")
-    .call(axis.set_tick_values([0, 120, 240, 360]))
-)
 
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
-
-# with open("light_axis_angle.svg", "w") as file:
-    # file.write(str(svg))
-
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(
-    d3.scale_linear().set_domain([0, 10_000]).set_range([0, width - 2 * margin])
-)
-x4 = (
-    svg.append("g")
-    .attr("transform", f"translate({margin}, {margin * 0.5})")
-    .call(axis.set_tick_format(d3.format("~s")))
-)
-
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
-
-# with open("light_axis_format.svg", "w") as file:
-    # file.write(str(svg))
-
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin]))
-x5 = (
-    svg.append("g")
-    .attr("transform", f"translate({margin}, {margin * 0.5})")
-    .call(axis.set_tick_size(12))
-)
-
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
-
-# with open("light_axis_tick_size.svg", "w") as file:
-    # file.write(str(svg))
-
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin]))
-x6 = (
-    svg.append("g")
-    .attr("transform", f"translate({margin}, {margin * 0.5})")
-    .call(axis.set_tick_size_inner(0))
-)
-
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
-
-# with open("light_axis_tick_size_inner.svg", "w") as file:
-    # file.write(str(svg))
-
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin]))
-x7 = (
-    svg.append("g")
-    .attr("transform", f"translate({margin}, {margin * 0.5})")
-    .call(axis.set_tick_size_outer(12))
-)
-
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
-
-# with open("light_axis_tick_size_outer.svg", "w") as file:
-    # file.write(str(svg))
-
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin]))
-x8 = (
-    svg.append("g")
-    .attr("transform", f"translate({margin}, {margin * 0.5})")
-    .call(axis.set_tick_padding(8))
-)
-
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
-
-# with open("light_axis_tick_padding.svg", "w") as file:
-    # file.write(str(svg))
-
-svg = (
-    d3.create("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("viewBox", f"0 0 {width} {height}")
-)
-axis = d3.axis_bottom(d3.scale_linear().set_range([0, width - 2 * margin]))
-x9 = (
-    svg.append("g")
-    .attr("transform", f"translate({margin}, {margin * 0.5})")
-    .call(axis.set_offset(8))
-)
-
-# To change color all in white
-# svg.select_all(".domain").attr("stroke", "white")
-# svg.select_all("g.tick").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("line").attr("fill", "none").attr("stroke", "white")
-# svg.select_all("text").style("fill", "white").style("stroke", "none")
-
-# with open("light_axis_offset.svg", "w") as file:
-    # file.write(str(svg))
+for title, axis in axes:
+    generate_svg(title, axis)
