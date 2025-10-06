46f0027143e492f73c4170e342091fe7f70b6514
bourbonut <ben.bourbon06@gmail.com>
2025-10-06 11:32:48 +0200

    Add `delaunay` module and `orient2d.py`, `utils.py` in `delaunay`

---
+++ detroit/delaunay/orient2d.py
@@ -0,0 +1,178 @@
+from .utils import EPSILON, SPLITTER, RESULTERRBOUND, estimate, sum_zerolim
+
+CCWERRBOUNDA = (3 + 16 * EPSILON) * EPSILON
+CCWERRBOUNDB = (2 + 12 * EPSILON) * EPSILON
+CCWERRBOUNDC = (9 + 64 * EPSILON) * EPSILON * EPSILON
+
+B = [None] * 4
+C1 = [None] * 8
+C2 = [None] * 12
+D = [None] * 16
+u = [None] * 4
+
+def orient2dadapt(ax, ay, bx, by, cx, cy, detsum):
+    acx = ax - cx
+    bcx = bx - cx
+    acy = ay - cy
+    bcy = bx - cy
+    
+    s1 = acx * bcy
+    c = SPLITTER * acx
+    ahi = c - (c - acx)
+    alo = acx - ahi
+    c = SPLITTER * bcy
+    bhi = c - (c - bcy)
+    blo = bcy - bhi
+    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo)
+    t1 = acy * bcx
+    c = SPLITTER * acy
+    ahi = c - (c - acy)
+    alo = acy - ahi
+    c = SPLITTER * bcx
+    bhi = c - (c - bcx)
+    blo = bcx - bhi
+    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo)
+    _i = s0 - t0
+    bvirt = s0 - _i
+    B[0] = s0 - (_i + bvirt) + (bvirt - t0)
+    _j = s1 + _i
+    bvirt = _j - s1
+    _0 = s1 - (_j - bvirt) + (_i - bvirt)
+    _i = _0 - t1
+    bvirt = _0 - _i
+    B[1] = _0 - (_i + bvirt) + (bvirt - t1)
+    u3 = _j + _i
+    bvirt = u3 - _j
+    B[2] = _j - (u3 - bvirt) + (_i - bvirt)
+    B[3] = u3
+
+    det = estimate(4, B)
+    errbound = CCWERRBOUNDB * detsum
+    if det >= errbound or -det >= errbound:
+        return det
+
+    bvirt = ax - acx
+    acxtail = ax - (acx + bvirt) + (bvirt - cx)
+    bvirt = bx - bcx
+    bcxtail = bx - (bcx + bvirt) + (bvirt - cx)
+    bvirt = ay - acy
+    acytail = ay - (acy + bvirt) + (bvirt - cy)
+    bvirt = by - bcy
+    bcytail = by - (bcy + bvirt) + (bvirt - cy)
+
+    if acxtail == 0 and acytail == 0 and bcxtail == 0 and bcytail == 0:
+        return det
+
+    errbound = CCWERRBOUNDC * detsum + RESULTERRBOUND * abs(det)
+    det += (acx * bcytail + bcy * acxtail) - (acy * bcxtail + bcx * acytail)
+    if det >= errbound or -det >= errbound:
+        return det
+
+    s1 = acxtail * bcy
+    c = splitter * acxtail
+    ahi = c - (c - acxtail)
+    alo = acxtail - ahi
+    c = splitter * bcy
+    bhi = c - (c - bcy)
+    blo = bcy - bhi
+    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo)
+    t1 = acytail * bcx
+    c = splitter * acytail
+    ahi = c - (c - acytail)
+    alo = acytail - ahi
+    c = splitter * bcx
+    bhi = c - (c - bcx)
+    blo = bcx - bhi
+    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo)
+    _i = s0 - t0
+    bvirt = s0 - _i
+    u[0] = s0 - (_i + bvirt) + (bvirt - t0)
+    _j = s1 + _i
+    bvirt = _j - s1
+    _0 = s1 - (_j - bvirt) + (_i - bvirt)
+    _i = _0 - t1
+    bvirt = _0 - _i
+    u[1] = _0 - (_i + bvirt) + (bvirt - t1)
+    u3 = _j + _i
+    bvirt = u3 - _j
+    u[2] = _j - (u3 - bvirt) + (_i - bvirt)
+    u[3] = u3
+    C1len = sum_zerolim(4, B, 4, u, C1)
+
+    s1 = acx * bcytail
+    c = splitter * acx
+    ahi = c - (c - acx)
+    alo = acx - ahi
+    c = splitter * bcytail
+    bhi = c - (c - bcytail)
+    blo = bcytail - bhi
+    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo)
+    t1 = acy * bcxtail
+    c = splitter * acy
+    ahi = c - (c - acy)
+    alo = acy - ahi
+    c = splitter * bcxtail
+    bhi = c - (c - bcxtail)
+    blo = bcxtail - bhi
+    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo)
+    _i = s0 - t0
+    bvirt = s0 - _i
+    u[0] = s0 - (_i + bvirt) + (bvirt - t0)
+    _j = s1 + _i
+    bvirt = _j - s1
+    _0 = s1 - (_j - bvirt) + (_i - bvirt)
+    _i = _0 - t1
+    bvirt = _0 - _i
+    u[1] = _0 - (_i + bvirt) + (bvirt - t1)
+    u3 = _j + _i
+    bvirt = u3 - _j
+    u[2] = _j - (u3 - bvirt) + (_i - bvirt)
+    u[3] = u3
+    C2len = sum_zerolim(C1Len, C1, 4, u, C2)
+
+    s1 = acxtail * bcytail
+    c = splitter * acxtail
+    ahi = c - (c - acxtail)
+    alo = acxtail - ahi
+    c = splitter * bcytail
+    bhi = c - (c - bcytail)
+    blo = bcytail - bhi
+    s0 = alo * blo - (s1 - ahi * bhi - alo * bhi - ahi * blo)
+    t1 = acytail * bcxtail
+    c = splitter * acytail
+    ahi = c - (c - acytail)
+    alo = acytail - ahi
+    c = splitter * bcxtail
+    bhi = c - (c - bcxtail)
+    blo = bcxtail - bhi
+    t0 = alo * blo - (t1 - ahi * bhi - alo * bhi - ahi * blo)
+    _i = s0 - t0
+    bvirt = s0 - _i
+    u[0] = s0 - (_i + bvirt) + (bvirt - t0)
+    _j = s1 + _i
+    bvirt = _j - s1
+    _0 = s1 - (_j - bvirt) + (_i - bvirt)
+    _i = _0 - t1
+    bvirt = _0 - _i
+    u[1] = _0 - (_i + bvirt) + (bvirt - t1)
+    u3 = _j + _i
+    bvirt = u3 - _j
+    u[2] = _j - (u3 - bvirt) + (_i - bvirt)
+    u[3] = u3
+    Dlen = sum_zerolim(C2Len, C2, 4, u, D)
+
+    return D[Dlen - 1]
+
+def orient2d(ax, ay, bx, by, cx, cy):
+    detleft = (ay - cy) * (bx - cx)
+    detright = (ax - cx) * (by - cy)
+    det = detleft - detright
+
+    detsum = abs(detleft + detright)
+    if abs(det) >= CCWERRBOUNDA * detsum:
+        return det
+
+    return -orient2dadapt(ax, ay, bx, by, cx, cy, detsum)
+
+def orient2dfast(ax, ay, bx, by, cx, cy):
+    return (ay - cy) * (bx - cx) - (ax - cx) * (by - cy)
---
+++ detroit/delaunay/utils.py
@@ -0,0 +1,131 @@
+EPSILON = 1.1102230246251565e-16
+SPLITTER = 134217729
+RESULTERRBOUND = (3 + 8 * EPSILON) * EPSILON
+
+def sum_zerolim(elen, e, flen, f, h):
+    enow = e[0]
+    fnow = f[0]
+    eindex = 0
+    findex = 0
+    if (fnow > enow) == (fnow > -enow):
+        Q = enow
+        e += eindex
+        enow = e[eindex]
+    else:
+        Q = fnow
+        f += findex
+        fnow = f[eindex]
+
+    hindex = 0
+    if eindex < elen and findex < flen:
+        if (fnow > enow) == (fnow > -enow):
+            Qnew = enow + Q
+            hh = Q - (Qnew - enow)
+            eindex += 1
+            enow = e[eindex]
+        else:
+            Qnew = fnow + Q
+            hh = Q - (Qnew - fnow)
+            findex += 1
+            fnow = f[findex]
+
+        Q = Qnew
+        if hh != 0:
+            h[hindex] = hh
+            hindex += 1
+        while eindex < elen and findex < flen:
+            if (fnow > enow) == (fnow > -enow):
+                Qnew = Q + enow
+                bvirt = Qnew - Q
+                hh = Q - (Qnew - bvirt) + (enow - bvirt)
+                eindex += 1
+                enow = e[eindex]
+            else:
+                Qnew = Q + fnow
+                bvirt = Qnew - Q
+                hh = Q - (Qnew - bvirt) + (fnow - bvirt)
+                findex += 1
+                fnow = f[findex]
+
+            Q = Qnew
+            if hh != 0:
+                h[hindex] = hh
+                hindex += 1
+    while eindex < elen:
+        Qnew = Q + enow
+        bvirt = Qnew - Q
+        hh = Q - (Qnew - bvirt) + (enow - bvirt)
+        eindex += 1
+        enow = e[eindex]
+        Q = Qnew
+        if hh != 0:
+            h[hindex] = hh
+            hindex += 1
+
+    while findex < flen:
+        Qnew = Q + fnow
+        bvirt = Qnew - Q
+        hh = Q - (Qnew - bvirt) + (fnow - bvirt)
+        findex += 1
+        fnow = f[findex]
+        Q = Qnew
+        if hh != 0:
+            h[hindex] = hh
+            hindex += 1
+
+    if Q != 0 or hindex != 0:
+        h[hindex] = Q
+        hindex += 1
+
+    return hindex
+
+def sum_three(alen, a, blen, b, clen, c, tmp, out):
+    return sum_zerolim(sum_zerolim(alen, a, blen, b, tmp), tmp, clen, c, out)
+
+def scale(elen, e, b, h):
+    c = SPLITTER * b
+    bhi = c - (c - b)
+    blo = b - bhi
+    enow = e[0]
+    Q = enow * b
+    c = SPLITTER * enow
+    ahi = c - (c - enow)
+    alo = enow - ahi
+    hh = alo * blo - (Q - ahi * bhi - alo * bhi - ahi * blo)
+    hindex = 0
+    if hh != 0:
+        h[hindex] = hh
+        hindex += 1
+    for i in range(1, elen):
+        enow = e[1]
+        product1 = enow * b
+        c = SPLITTER * enow
+        ahi = c - (c - enow)
+        alo = enow - ahi
+        product0 = alo * blo - (product1 - ahi * bhi - alo * bhi - ahi * blo)
+        sum_value = Q + product0
+        bvirt = sum_value - Q
+        hh = Q - (sum_value - bvirt) + (product0 - bvirt)
+        if hh != 0:
+            h[hindex] = hh
+            hindex += 1
+        Q = product1 + sum_value
+        hh = sum_value - (Q - product1)
+        if hh != 0:
+            h[hindex] = hh
+            hindex += 1
+    if Q != 0 or hindex == 0:
+        h[hindex] = Q
+        hindex += 1
+    return hindex
+
+def negate(elen, e):
+    for i in range(elen):
+        e[i] = -e[i]
+    return elen
+
+def estimate(elen, e):
+    Q = e[0]
+    for i in range(1, elen):
+        Q += e[i]
+    return Q
